4671d272ae85ef49b37c96158f695271bd57af65
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-09 14:29:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,7 @@ def introduction(name)
   pusts "Hi, mya name is #{name}" 
   
 end
+
+def introduction_with_language(name, language)
+  puts "Hi my name is #{name} and i am learning to program in #{language}"
+end
