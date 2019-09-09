52149797bfc4970019816591e711fcd653b9ace4
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-09 14:31:53 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  pusts "Hi, my name is #{name}" 
+  
+end
+introduction()
+def introduction_with_language(name, language)
+  puts "Hi my name is #{name} and i am learning to program in #{language}"
+end
+introduction_with_language("Maynard", "ruby")
